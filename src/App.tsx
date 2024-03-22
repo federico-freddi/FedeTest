027bff3bdd8e02ddc7578d6ef80c87a3427d919e
Federico Freddi <124000867+federico-freddi@users.noreply.github.com>
2024-03-22 10:16:59 +0000

    New Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 // @ts-ignore
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // @ts-ignore
-import Dashboard from "./Components/Dashboard/Dashboard.jsx"
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 // @ts-ignore
-import UserDetailsPage from "./Components/UsersTable/UserDetailsPage.jsx"
-import './App.css'
+import UserDetailsPage from "./Components/UsersTable/UserDetailsPage.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <BrowserRouter>
-         
-    <Routes>
-      <Route path="/" element={<Dashboard/>} />
-      {/* <Route path="/user-details/:userId" element={<UserDetailsPage/>} /> */}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
-    
-  )
+  );
 }
 
-export default App
+export default App;
