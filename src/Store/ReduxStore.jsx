6db53136eeb37a721f84a3e08a6e3811b5dd6938
Federico Freddi <124000867+federico-freddi@users.noreply.github.com>
2024-03-22 10:04:26 +0000

    commit

--- src/Store/ReduxStore.jsx
+++ src/Store/ReduxStore.jsx
@@ -1,8 +1,9 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import rootReducer from './RootReducer';
-
+// @ts-ignore
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// @ts-ignore
+import rootReducer from "./RootReducer";
 
 const store = configureStore({
   reducer: rootReducer,
-})
+});
 export default store;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// @ts-ignore
 import { Provider } from "react-redux";
 // @ts-ignore
 import store from "./Store/ReduxStore.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+);
